a627f341011f43ca85507d9225fe3cb1cd171360
taitongngocanh <taitongngocanh@gmail.com>
2025-08-02 16:16:08 +0700

    day 2

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -2,6 +2,13 @@ import TodoRowItem from './components/TodoRowItem';
 import './App.css';
 
 function App() {
+
+  const todos = [
+    {rowNumber: 1, rowDescription: 'Feed monkey', rowAssigned: 'Anh Tai'},
+    {rowNumber: 2, rowDescription: 'Do react', rowAssigned: 'Chi Pheo'},
+    {rowNumber: 3, rowDescription: 'Have a shower', rowAssigned: 'Thi No'}
+  ]
+
   return (
     <div className="mt-5 container">
       <div>
@@ -20,12 +27,9 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            <TodoRowItem />
-            <tr>
-              <td>2</td>
-              <td>Build a Todo App</td>
-              <td>Jane Smith</td>
-            </tr>
+            <TodoRowItem rowNumber={todos[0].roleNumber} rowDescription={todos[0].rowDescription} rowAssigned={todos[0].rowAssigned}/>
+            <TodoRowItem rowNumber={todos[1].roleNumber} rowDescription={todos[1].rowDescription} rowAssigned={todos[1].rowAssigned}/>
+            <TodoRowItem rowNumber={todos[2].roleNumber} rowDescription={todos[2].rowDescription} rowAssigned={todos[2].rowAssigned}/>       
           </tbody>
         </table>
       </div>
--- react-todo-demo/react-todos/src/components/TodoRowItem.jsx
+++ react-todo-demo/react-todos/src/components/TodoRowItem.jsx
@@ -1,9 +1,10 @@
-function TodoRowItem() {
+function TodoRowItem(props) {
+
      return (
                <tr>
-                    <th scope='row'>1</th>
-                    <td>Learn React</td>
-                    <td>Anh Tai</td>
+                    <th scope='row'>{props.rowNumber}</th>
+                    <td>{props.rowDescription}</td>
+                    <td>{props.rowAssigned}</td>
                </tr> 
      )
 }
