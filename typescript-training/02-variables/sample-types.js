dc2a8f27da45eef39327fa1d16d8f862050c80f0
taitongngocanh <taitongngocanh@gmail.com>
2025-08-06 16:43:14 +0700

    day 4 - update 9

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,14 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Tai";
+var lastName = "Tong";
+// Let's break it
+// found = 0;
+// grade = "A";
+// firstName = false;
+console.log(found);
+console.log(grade);
+console.log(firstName);
+console.log(lastName);
+// use template string
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,20 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Tai";
+let lastName: string = "Tong";
+
+// Let's break it
+
+
+// found = 0;
+// grade = "A";
+// firstName = false;
+
+
+console.log(found);
+console.log(grade);
+console.log(firstName);
+console.log(lastName)
+
+// use template string
+console.log(`Hi ${firstName} ${lastName}`);
