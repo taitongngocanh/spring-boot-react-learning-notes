de866c6fca5ac4680da9c57360a2f6af7a5f6ff9
taitongngocanh <taitongngocanh@gmail.com>
2025-08-07 15:02:11 +0700

    day 5 - update 6

---
+++ typescript-training/04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,15 @@
+class Customer {
+     firstName: string;
+     lastName: string;
+
+     constructor(theFirst: string, theLast: string) {
+          this.firstName = theFirst;
+          this.lastName = theLast;
+     }
+}
+
+// Let's create an instance
+let myCustomer = new Customer("Tai", "Nguyen");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
