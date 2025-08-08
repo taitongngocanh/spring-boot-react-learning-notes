2c96b7bc5260bb68b61aede53053e364df044bca
taitongngocanh <taitongngocanh@gmail.com>
2025-08-08 15:39:17 +0700

    day 6 - update 6

--- typescript-training/05_inheritance/Driver.ts
+++ typescript-training/05_inheritance/Driver.ts
@@ -1,8 +1,12 @@
 import { Shape } from "./Shape";
 import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
 
 let myShape = new Shape(10, 15);
 console.log(myShape.getInfo());
 
 let myCircle = new Circle(5, 10, 34);
 console.log(myCircle.getInfo());
+
+let myRectangle = new Rectangle(3, 4, 5, 3);
+console.log(myRectangle.getInfo());
